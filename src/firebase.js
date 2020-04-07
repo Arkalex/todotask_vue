0d15bf100270775d43d4817ed2267e3a7da1d154
Alex <aayllon@pasiona.com>
2020-04-07 15:07:09 +0200

    initialize project

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfXyhOQZ1RVmwqUEGezf1KSMKTMqLHiyk",
+    authDomain: "helloworld-e6a23.firebaseapp.com",
+    databaseURL: "https://helloworld-e6a23.firebaseio.com",
+    projectId: "helloworld-e6a23",
+    storageBucket: "helloworld-e6a23.appspot.com",
+    messagingSenderId: "266667745375",
+    appId: "1:266667745375:web:cd09cc6d203dd4fdd2fbea"
+};
+
+firebase.initializeApp(firebaseConfig);
